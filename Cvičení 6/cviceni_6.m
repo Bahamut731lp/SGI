4de38c172210c8e65afccd14b4479831eff69ba9
Kevin Daněk <heindrich73@gmail.com>
2022-10-31 14:34:40 +0100

    Cvičení 6

---
+++ Cvičení 6/cviceni_6.m
@@ -0,0 +1,30 @@
+clear all;
+close all;
+clc;
+
+data = audioread("ovcaci-housle.wav");
+metadata = audioinfo("ovcaci-housle.wav");
+
+sampleCount = 256;
+overlap = 128;
+overlapCoefficient = sampleCount / overlap;
+
+zcr = getZeroCrossingRate(data, sampleCount, overlap);
+
+figure;
+tiledlayout(3, 1);
+
+nexttile;
+
+dataTime = (0:metadata.TotalSamples - 1) / metadata.SampleRate;
+plot(dataTime, data);
+
+nexttile;
+time = (0:overlap:metadata.TotalSamples - 1);
+time = time(1:end - 1);
+time = time / metadata.SampleRate;
+
+plot(time, zcr);
+
+nexttile;
+spectrogram(data, hamming(256), 128, 256, metadata.SampleRate, 'yaxis');
