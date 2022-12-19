70a1d67f68d13875cec28e4aedb7298162c07099
Kevin Daněk <heindrich73@gmail.com>
2022-12-19 16:59:06 +0100

    Update Cvičení 12

--- Cvičení 12/Uloha1.m
+++ Cvičení 12/Uloha1.m
@@ -1,11 +1,11 @@
 clear all;
 % data 
-test_soubor = 'c6_p0001_v02.jpg';
+test_soubor = 'c3_p0001_v01.jpg';
 tren_soubor = ['c5_p0001_v02.jpg';'c6_p0001_v02.jpg';'c7_p0001_v02.jpg';'c8_p0001_v02.jpg';'c9_p0001_v02.jpg';
                'c0_p0001_v02.jpg';'c1_p0001_v02.jpg';'c2_p0001_v02.jpg';'c3_p0001_v02.jpg';'c4_p0001_v02.jpg'];
 
-load("digits_tren.mat");
-
+%load("digits_tren.mat");
+tren_trida = [5, 6, 7, 8, 9, 0, 1, 2, 3, 4];
 % porovnani vzdalenosti
 x = imread(test_soubor);
 for j = 1:10
